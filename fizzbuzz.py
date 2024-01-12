c7cc34c14b67cfe148fee41985cb97c8f68c51d4
LazyLEE1008 <66935871+LazyLEE1008@users.noreply.github.com>
2024-01-12 16:33:04 +0900

    feat: Print fizz or buzz
    Print fizz if i is multiple of 3.
    print buzz if i is multiple of 5.
    Otherwise, print i itself.

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+def do_fizzbuzz():
+    for i in ragne(1,15+1):
+        if i%3==0:
+            print('fizz')
+        elif i%5==0:
+            print('buzz')
+        else :
+            print(f'{i}')
+
+
+
+if __name__=='__main__':
+    do_fizzbuzz()
+
+
+
